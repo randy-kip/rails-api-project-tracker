287df0d3fce9ada9f6947faf517bc8015e4df07e
Randy Kipkurui <randy.kipkurui4@gmail.com>
2023-12-16 17:35:01 +0300

    1. feat: Add signin and signup routes. 2. feat: Add authentication in the Application Controller. 3. feat Add authenticate action and other prerequisites to the Authentication Controller.

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,2 +1,17 @@
 class AuthenticationController < ApplicationController
+    skip_before_action :verify_authenticity_token, :only => :authenticate
+    skip_before_action :authorize_request, only: :authenticate, raise: false
+
+    # /signin
+    def authenticate
+        auth_token =
+        AuthenticateUser.new(auth_params[:email], auth_params[:password]).call
+        json_response(auth_token: auth_token)
+    end
+
+    private
+
+    def auth_params
+        params.permit(:email, :password)
+    end
 end
