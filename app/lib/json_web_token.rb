b597177a88a14fa98632ccadbe013ccc2ab5152f
Randy Kipkurui <randy.kipkurui4@gmail.com>
2023-12-16 18:42:09 +0300

    1. feat: Create AuthorizeAPIRequest class for authorization. 2. feat: Modify projects controller for the currentuser to be able to create a project. 3. feat: Create ExceptionHandler and Reponse modules to be used by the controllers. 4.  refactor: Correct errors in the jsonwebtoken class. 5. Create a Mesage class to hold error messages.

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -2,7 +2,7 @@ class JsonWebToken
     # secret to encode and decode token
     HMAC_SECRET = Rails.application.secrets.secret_key_base
 
-    def self.encode(payloadd, exp = 24.hours.from_now)
+    def self.encode(payload, exp = 24.hours.from_now)
         # set expiry to 24 hours from creation time
         payload[:exp] = exp.to_i
         # sign token with application secret
@@ -14,9 +14,8 @@ class JsonWebToken
         body = JWT.decode(token, HMAC_SECRET)[0]
         HashWithIndifferentAccess.new body
         # rescue from all decode errors
-        rescue JWT::DecodeError => e
-            # raise custom error to be handled by custom handler
-            raise ExceptionHandler::InvalidToken, e.message
-        end
-    end
+      rescue JWT::DecodeError => e
+        # raise custom error to be handled by custom handler
+        raise ExceptionHandler::InvalidToken, e.message
+      end
 end
