d3d60db442bc4c7928964c00f257ad2a620a8b00
Randy Kipkurui <randy.kipkurui4@gmail.com>
2023-12-18 14:15:09 +0300

    feat: Serializing the Project model to add custom data.

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,6 +5,21 @@ class Project < ApplicationRecord
     validates_presence_of :title, :created_by
 
     def as_json(options = nil)
-        super(:include => [:milestones])
+        hash = super (: include => {:milestones => {:except => [:updated_at, :project_id]}})
+        hash.store(:project_title, hash.delete("title"))
+
+        hash [:created_at] = self.created_at.to_time.to_i
+        hash [:updated_at] = self.updated_at.to_time.to_i
+        hash [:start_date] = self.updated_at.to_time.to_i
+        hash [:end_date] = self.updated_at.to_time.to_i
+
+        hash [:is_single_day_project] = single_day_project
+        return hash
+    end
+
+    private
+
+    def single_day_project
+        Time.at(self.start_date).to_date === Time.at(self.end_date).to_date
     end
 end
